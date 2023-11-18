851008a5b1fddb2e836ff23d5ac18ac5392d9b7e
Cade Weiskopf <cadew@pm.me>
2023-11-18 15:03:55 -0500

    init

---
+++ types.ts
@@ -0,0 +1,29 @@
+import emailValidator from "email-validator";
+
+export type IEmailData = {
+  firstName: string;
+  lastName: string;
+  companyName?: string;
+  email: string;
+  phone?: string;
+};
+
+export const isIEmailData = (data: any): data is IEmailData => {
+  if (
+    typeof data !== "object" ||
+    typeof data.firstName !== "string" ||
+    data.firstName.trim().length <= 0 ||
+    typeof data.lastName !== "string" ||
+    data.lastName.trim().length <= 0 ||
+    (data.companyName !== undefined && typeof data.companyName !== "string") ||
+    (data.companyName !== undefined && data.companyName.length <= 0) ||
+    typeof data.email !== "string" ||
+    data.email.trim().length <= 0 ||
+    emailValidator.validate(data.email) ||
+    (data.phone !== undefined && typeof data.phone !== "string")
+  ) {
+    return false;
+  }
+
+  return true;
+};
