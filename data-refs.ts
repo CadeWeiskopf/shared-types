d70f7a14104f40288f03cfc7db217acca548340b
Cade Weiskopf <cadew@pm.me>
2023-11-18 23:10:28 -0500

    generic getDataRef, and minor code format

--- data-refs.ts
+++ data-refs.ts
@@ -1,7 +1,7 @@
 import { RefObject } from "react";
 
-export type DataRef = {
-  ref: RefObject<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>;
+export type DataRef<T extends HTMLElement = HTMLElement> = {
+  ref: RefObject<T>;
   refName: string;
 };
 
@@ -10,18 +10,25 @@ export const generateObject = <T>(
   dataRefs: DataRef[]
 ): T => {
   const obj: Record<string, unknown> = {};
-
   dataRefs.forEach((dataRef) => {
-    if (dataRef.ref.current) {
-      obj[dataRef.refName] = dataRef.ref.current.value;
-    } else {
+    if (!dataRef.ref.current) {
       throw new Error(`Missing ref: ${dataRef.refName}`);
     }
+    obj[dataRef.refName] = dataRef.ref.current.value;
   });
-
   if (!typeGuard(obj)) {
     throw Error(`generateObject failed typeGuard: ${JSON.stringify(obj)}`);
   }
-
   return obj as T;
+};
+
+export const getDataRef = <T extends HTMLElement = HTMLElement>(
+  refName: string,
+  dataRefs: DataRef[]
+): DataRef<T> => {
+  const dataRef = dataRefs.find((e) => e.refName === refName);
+  if (!dataRef) {
+    throw Error(`No dataRef in dataRefs with refName=${refName}`);
+  }
+  return dataRef as DataRef<T>;
 };
